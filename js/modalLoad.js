7a33e281f3e663d76636e07612628e128e8773de
joostburgers <joostburgers@gmail.com>
2023-07-21 05:55:36 -0400

    create video modal json

--- js/modalLoad.js
+++ js/modalLoad.js
@@ -1,56 +1,124 @@
-// JavaScript source code
-var imageData = null; // global variable to store image metadata
-
-var imageDataURL = 'https://raw.githubusercontent.com/joostburgers/teaching_and_learning_mockup/master/data/imageData.json'
-
 function loadImageData() {
-	$.ajax({
+	const imageDataURL = 'https://raw.githubusercontent.com/joostburgers/teaching_and_learning_mockup/master/data/imageData.json';
 
-		url: imageDataURL,
-		dataType: 'json',
-		async: false,
-
-		success: function (data) {
-
-		}
-
+	$.getJSON(imageDataURL, function (jsonData) {
+		$('.activity-image img').click(function () {
+			const source = $(this).attr('src');
+			const tempImageData = getImageData(source, jsonData);
+			setImageData(source, tempImageData);
+			$("#imageModal").show();
+		});
 	});
 
-}
-
-loadImageData();
-///This is not running properly. Need to get JSON sync?
-
-
-console.log(imageData)
-$(".activity-image img").click(function () {
-	var imageName = $(this).attr("src"); // get the image source
-	var imageMetadata = null;
-
-	imageData.forEach(function (obj) {
-		if (obj.imageName === imageName) {
-			// found matching image metadata
-			imageMetadata = obj;
-			return false; // exit loop early
-		}
+	$(".close").click(function () {
+		$("#imageModal").hide();
 	});
 
-	var modal = $("#imageModal");
-	var modalImage = $("#modalImage");
-	var imageCaption = $("#imageCaption");
-
-	modalImage.attr("src", $(this).attr("src"));
-	//captionText.text($(this).attr("alt"));
-
-	if (imageMetadata !== null) {
-		imageCaption.text("Description: " + imageMetadata.description + ", Author: " + imageMetadata.author + ", Image file name: " + imageMetadata.imageName);
-	} else {
-		metadataField.text("Metadata not found");
+	function getImageData(source, jsonData) {
+		const currentFilename = source.substring(source.lastIndexOf('/') + 1);
+		return jsonData.find(object => object.filename === currentFilename);
 	}
 
-	modal.show();
-});
+	function setImageData(source, image) {
+		const imageTitle = $('#imageTitle');
+		const imageSource = $('#imageSource');
+		const imageCitation = $('#imageCitation')
 
-$(".close").click(function () {
-	$("#myModal").hide();
-});
+		const creators = image.creators.map((creator, index) => {
+			if (index === 0) {
+				return `${creator.last_name}, ${creator.first_name}`;
+			}
+			return `${creator.first_name} ${creator.last_name}`;
+		});
+		const creatorsString = creators.join(', ');
+
+		let citationTemplate;
+
+		switch (image.media_type) {
+			case 'site_photograph':
+				citationTemplate =
+					`${creatorsString}.
+          "${image.image_title}." ${image.year}, ${image.image_place}. ${image.repository_name}, ${image.repository_place}. URL: <a href="${image.repository_url}">${image.repository_url}</a>`;
+				break;
+			case 'external_image':
+				citationTemplate =
+					`${creatorsString}. "${image.image_title}." <em>${image.website_title}</em>, ${image.year}, <a href="${image.original_url}">${image.original_url}</a>. Accessed ${new Date(image.access_date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}.`;
+				break;
+			case 'screen_capture':
+				citationTemplate =
+					`${creatorsString}. "${image.image_title}."
+            ${image.year}.
+          <em>${image.repository_name},</em> ${image.repository_place}, 
+          <a href="${image.repository_url}">${image.repository_url}</a>`;
+				break;
+			default:
+				// Default case if image type is not provided
+				break;
+		}
+
+		imageTitle.html(image.alt_text);
+		imageSource.attr('src', source);
+		imageSource.attr('alt', image.alt_text);
+		imageCitation.html(citationTemplate);
+
+	}
+}
+
+
+function loadVideoData() {
+	const videoDataURL = 'https://raw.githubusercontent.com/joostburgers/teaching_and_learning_mockup/master/data/videoData.json';
+
+	$.getJSON(videoDataURL, function (jsonData) {
+		$('.activity-video img').click(function () {
+			const source = $(this).attr('src');
+			const tempVideoData = getVideoData(source, jsonData);
+			setVideoData(source, tempVideoData);
+			$("#videoModal").show();
+		});
+	});
+
+	$(".close").click(function () {
+		$("#videoModal").hide();
+	});
+
+	function getVideoData(source, jsonData) {
+		const currentFilename = source.substring(source.lastIndexOf('/') + 1);
+		return jsonData.find(object => object.filename === currentFilename);
+	}
+
+	function setVideoData(source, video) {
+		const videoTitle = $('#videoTitle');
+		const videoSource = $('#videoSource');
+		const videoCitation = $('#videoCitation')
+
+		const creators = video.creators.map((creator, index) => {
+			if (index === 0) {
+				return `${creator.last_name}, ${creator.first_name}`;
+			}
+			return `${creator.first_name} ${creator.last_name}`;
+		});
+		const creatorsString = creators.join(', ');
+
+		let citationTemplate = '';
+
+		switch (video.media_type) {
+			case 'quick_tutorial':
+				citationTemplate =
+					`${creatorsString}.
+          "${video.video_title}." ${video.date}. ${video.institution_place}: ${video.institution}, ${video.course_title}.`;
+				break;
+			case 'external_video':
+				citationTemplate =
+					`${creatorsString}. "${video.video_title}." <em>${video.website_title}</em>, ${video.date}, <a href="${video.original_url}">${video.original_url}</a>. Accessed ${new Date(video.access_date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}.`;
+				break;
+			default:
+				// Default case if video type is not provided
+				break;
+		}
+
+		videoTitle.html(video.video_title);
+		videoSource.attr('src', source);
+		videoCitation.html(citationTemplate);
+
+	}
+}
